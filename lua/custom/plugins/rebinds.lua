e8ba7c7cba7350ca8ea2ed4bfb217a5fa96eb53f
Jan <LeopoldJan@t-online.de>
2025-08-13 16:13:08 +0200

    basic config with some plugins added and changed some keybindings

---
+++ lua/custom/plugins/rebinds.lua
@@ -0,0 +1,7 @@
+return {},
+  vim.keymap.set('n', '<leader>gv', vim.cmd.Ex, { desc = 'opens the file explorer' }),
+  -- move line(s) up or down
+  vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line/s down' }),
+  vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line/s up' }),
+  -- search and replace word under curser
+  vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'search and replace word' })
