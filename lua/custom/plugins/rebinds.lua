464d4bd74f78ab490a46210b72fe0da5ac16ad78
Jan Leopold <leopoldjan@t-online.de>
2025-10-26 09:08:23 +0100

    removed conflict keybinding, search and rename word under cursor is now <leader>r

--- lua/custom/plugins/rebinds.lua
+++ lua/custom/plugins/rebinds.lua
@@ -4,4 +4,4 @@ return {},
   vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line/s down' }),
   vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line/s up' }),
   -- search and replace word under curser
-  vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'search and replace word' })
+  vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'search and replace word' })
