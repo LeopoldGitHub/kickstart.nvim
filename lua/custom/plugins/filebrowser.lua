85b522633c1e95282d2517e8121bb97bb73df5bb
Jan Leopold <leopoldjan@t-online.de>
2026-01-13 00:13:59 +0100

    unified rebinds into one file, added oil.nvim and implemented a dir search with <leader>sD

---
+++ lua/custom/plugins/filebrowser.lua
@@ -0,0 +1,6 @@
+return {
+  'nvim-telescope/telescope-file-browser.nvim',
+  'nvim-tree/nvim-web-devicons',
+  -- 'ibhagwan/fzf-lua',
+  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+}
--- lua/custom/plugins/rebinds.lua
+++ lua/custom/plugins/rebinds.lua
@@ -1,7 +1,22 @@
 return {},
-  vim.keymap.set('n', '<leader>gv', vim.cmd.Ex, { desc = 'opens the file explorer' }),
+  vim.keymap.set('n', '<leader>gv', '<cmd>Oil<CR>', { desc = 'opens the file explorer' }),
   -- move line(s) up or down
   vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line/s down' }),
   vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line/s up' }),
   -- search and replace word under curser
-  vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'search and replace word' })
+  vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'search and replace word' }),
+  vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>', { desc = 'goes to next quickfix' }),
+  vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>', { desc = 'goes to prev quickfix' }),
+  vim.keymap.set({ 'n', 't' }, '<leader>tt', '<cmd>Floatterminal<return>', { desc = 'opens a floating terminal' }),
+  vim.keymap.set('n', 'q', '<Nop>', { desc = 'Disable macro recording' }),
+  vim.keymap.set('n', '<leader>xe', '<cmd>source %<CR>', { desc = 'Execute current file' }),
+  vim.keymap.set('n', '<leader>xx', '<cmd>!chmod +x %<CR>', { desc = 'Make file [x]ecutable' }),
+  -- vim.keymap.set({ 'n', 'v', 'V' }, 'y', '"ay', { noremap = true, desc = 'Yank into register "a"' }),
+  -- vim.keymap.set({ 'n', 'v', 'V' }, 'p', '"ap', { noremap = true, desc = 'Paste from Yank Register "a"' }),
+  -- vim.keymap.set({ 'n', 'v', 'V' }, 'P', '"aP', { noremap = true, desc = 'Paste from Yank Register "a"' }),
+  -- vim.keymap.set({ 'n', 'v', 'V' }, 'd', '"bd', { noremap = true, desc = 'Delete into register "b"' }),
+  -- vim.keymap.set('n', '<C-p>', '"bp', { noremap = true, desc = 'Paste from Delete Register "b"' }),
+  -- vim.keymap.set('n', '<C-P>', '"bP', { noremap = true, desc = 'Paste from Delete Register "b"' }),
+  vim.keymap.set('n', '<leader>sD', function()
+    require('custom.utils.findDirs').dirFinder()
+  end, { desc = '[s]earch [D]irs' })
